5cf1980ae82c9baa813f03646ab4118080ff75c4
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-05-10 19:14:38 -0300

    Criada animações em auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [ 
+        style({
+          opacity: 0,
+          transform: 'translate(-50px, 0)'
+        }),
+        animate('500ms 250ms ease-in-out') // duração, delay e aceleração
+      ])
+    ]),
+    trigger('animacao-card', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(50px, 0)'
+        }),
+        animate('500ms 250ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AuthComponent implements OnInit {
+
+  public estadoBanner: string = 'criado';
+  public estadoCard: string = 'criado';
+
+  public cadastro: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public exibirPainel(event: string): void {
+    this.cadastro = event === 'cadastro' ? true : false;
   }
 
 }
--- src/app/auth/banner/banner.component.ts
+++ src/app/auth/banner/banner.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
+import { Imagem } from './imagem.model';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('banner', [
+      state('invisivel', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('invisivel <=> visivel', animate('1s ease-in')),
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'visivel'
+
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '/assets/banner-auth/img_1.png' },
+    { estado: 'invisivel', url: '/assets/banner-auth/img_2.png' },
+    { estado: 'invisivel', url: '/assets/banner-auth/img_3.png' },
+    { estado: 'invisivel', url: '/assets/banner-auth/img_4.png' },
+    { estado: 'invisivel', url: '/assets/banner-auth/img_5.png' },
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.logicaRotacao()
+    }, 3000);
+  }
+
+  public logicaRotacao(): void {
+
+    // Auxilia na exibição da imagem seguinte
+    let idx: number;
+
+    // Ocultando imagem
+    for (let i: number = 0; i <= this.imagens.length - 1; i++) {
+      if (this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'invisivel'
+
+        idx = i === 4 ? 0 : i + 1
+
+        break
+      }
+    }
+
+    // Exibir próxima imagem
+    this.imagens[idx].estado = 'visivel'
+
+    // Loop com delay de 3s
+    setTimeout(() => {
+      this.logicaRotacao()
+    }, 3000);
   }
 
 }
