924f435fbc96487714122d221a3718f2f21e2450
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-05-10 22:09:26 -0300

    animação no card em auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-auth',
@@ -10,12 +10,10 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
       state('criado', style({
         opacity: 1
       })),
-      transition('void => criado', [ 
-        style({
-          opacity: 0,
-          transform: 'translate(-50px, 0)'
-        }),
-        animate('500ms 250ms ease-in-out') // duração, delay e aceleração
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-50px, 0)' }),
+        animate('500ms 250ms ease-in-out',
+        ) // duração, delay e aceleração
       ])
     ]),
     trigger('animacao-card', [
@@ -23,11 +21,20 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
         opacity: 1
       })),
       transition('void => criado', [
-        style({
-          opacity: 0,
-          transform: 'translate(50px, 0)'
-        }),
-        animate('500ms 250ms ease-in-out')
+        style({ opacity: 0, transform: 'translate(50px, 0)' }),
+        animate('1s 250ms ease-in-out',
+          keyframes([
+            style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+
+            style({ offset: 0.40, opacity: 1, transform: 'translateY(15px)' }),
+            style({ offset: 0.60, opacity: 1, transform: 'translateY(-15px)' }),
+            style({ offset: 0.75, opacity: 1, transform: 'translateX(0)' }),
+            style({ offset: 0.85, opacity: 1, transform: 'translateY(-5px)' }),
+            style({ offset: 0.95, opacity: 1, transform: 'translateY(5px)' }),
+
+            style({ offset: 1, opacity: 1, transform: 'translateX(0)' }),
+          ])
+        )
       ])
     ])
   ]
@@ -46,6 +53,14 @@ export class AuthComponent implements OnInit {
 
   public exibirPainel(event: string): void {
     this.cadastro = event === 'cadastro' ? true : false;
+  }
+
+  public inicioDaAnimacao(): void {
+    console.log('inicio da animação');
+  }
+
+  public fimDaAnimacao(): void {
+    console.log('fim da animação');
   }
 
 }
